1db73494f68ec700fd17dbc10e54325d75ca352a
SiddeshShimpi25 <72143997+SiddeshShimpi25@users.noreply.github.com>
2020-10-03 13:20:23 +0530

    Update q1_visitor_counter.php

--- Day4/q1_visitor_counter.php
+++ Day4/q1_visitor_counter.php
@@ -3,8 +3,8 @@ error_reporting(0);
 $conn = mysqli_connect("localhost","root","","visitor_counter") or die(mysqli_connect_error());
 $extract = mysqli_query($conn,"SELECT * FROM counter where id=1");
 while($row=mysqli_fetch_assoc($extract)){
-    $count=$row['count'];
+    $count=$row['count']; //database already has count value as 0.
 }
-echo "You have $count visitors";
 $extract = mysqli_query($conn,"UPDATE counter SET count=$count+1 where id=1");
+echo "You have $count visitors";
 ?>
